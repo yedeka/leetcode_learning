9323c5728585155feadcbb1ac90b8c33614ab113
yedekar3 <Yogi#2606>
2021-06-02 07:04:00 -0700

    Adding logic for remove duplicates II.

---
+++ Leetcode/src/com/leetcode/preps/arrays/RemoveDuplicatesII.java
@@ -0,0 +1,23 @@
+package com.leetcode.preps.arrays;
+
+public class RemoveDuplicatesII {
+    public static int removeDuplicates(int[] nums) {
+    int j = 1, count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+            if (count <= 2) {
+                nums[j++] = nums[i];
+            }
+        }
+        return j;
+    }
+    public static void main(String[] args){
+        int[] input = new int[] {1,1,2,3,3,3,3};
+        int DoubleduplicateSize = removeDuplicates(input);
+    }
+
+}
