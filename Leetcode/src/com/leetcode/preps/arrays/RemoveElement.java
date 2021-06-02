a0a79deec93b65a654a0538ba04c7959d7f62cdc
yedekar3 <Yogi#2606>
2021-06-02 03:18:42 -0700

    Adding code for remove specified element from array problem.

--- Leetcode/src/com/leetcode/preps/arrays/RemoveElement.java
+++ Leetcode/src/com/leetcode/preps/arrays/RemoveElement.java
@@ -11,10 +11,28 @@ public class RemoveElement {
         }
         return i;
     }
+
+    private static int removeRare(int[] nums, int val){
+        int i=0, n = nums.length;
+        while(i< n){
+            if(nums[i] == val){
+                nums[i] = nums[n-1];
+                n --;
+            }else{
+                i++;
+            }
+        }
+        return n;
+    }
+
     public static void main(String[] args){
         int[] arr = new int[]{3,2,2,3};
         int val = 3;
         int newsize = removeElement(arr, val);
+        System.out.println("Size of new array => "+newsize);
+        arr = new int[]{4,5,3,2,1,0};
+        val = 4;
+        newsize = removeRare(arr, val);
         System.out.println("Size of new array => "+newsize);
     }
 }
